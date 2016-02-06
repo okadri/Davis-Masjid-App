51c0b12ac8d3aba62725013d8161d17d2ea9e2f1
Obada Kadri <okadri@ucdavis.edu>
2016-02-06 00:51:33 -0800

    Fixes XML parsing URL, and removes extra timestamp details

--- www/js/app.js
+++ www/js/app.js
@@ -25,6 +25,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
   $ionicConfigProvider.tabs.position('bottom');
+  $ionicConfigProvider.navBar.alignTitle('center');
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
--- www/js/services.js
+++ www/js/services.js
@@ -23,7 +23,7 @@ angular.module('starter.services', [])
         get : function(feedUrl){
             var defered = $q.defer();
 
-            $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(feedUrl))
+            $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(feedUrl))
             .success(function(feed){
                 defered.resolve(feed);
             })
